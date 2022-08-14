797cf1d5f8b516b2da61e8934317108e07460adf
ntieng <noveltieng@gmail.com>
2022-08-14 16:37:15 +0800

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using System.Runtime.InteropServices;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serverAddress = "https://localhost:5001";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // The following statement allows you to call insecure services.
+                // To be used only in development environments or If it’s running on macOS or Win 7 and below.
+                AppContext.SetSwitch(
+                    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                serverAddress = "http://localhost:5000";
+            }
+
+            // instantiate the Client
+            using var channel = GrpcChannel.ForAddress(serverAddress);
+            var client = new Author.AuthorClient(channel);
+            var reply = await client.GetAuthorAsync(new AuthorRequest { Name = "Antonio Gonzalez" });
+
+            Console.WriteLine("Author: " + reply.ToString());
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcServer/Services/AuthorService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using GrpcAuthor;
+
+namespace GrpcSample.Services
+{
+    public class AuthorService : Author.AuthorBase
+    {
+        private readonly ILogger<AuthorService> _logger;
+        private List<AuthorResponse> authors;
+
+        public AuthorService(ILogger<AuthorService> logger)
+        {
+            _logger = logger;
+            authors = new List<AuthorResponse>();
+
+            var antonio = new AuthorResponse { Name = "Antonio Gonzalez" };
+            antonio.BookAuthored.Add(new BookReply { Title = "Much Ado about nothing" });
+            antonio.BookAuthored.Add(new BookReply { Title = "How to do a split" });
+            antonio.BookAuthored.Add(new BookReply { Title = "Sample Book" });
+            authors.Add(antonio);
+
+            var jack = new AuthorResponse { Name = "Jack Olabisi" };
+            jack.BookAuthored.Add(new BookReply { Title = "Early morning bird" });
+            jack.BookAuthored.Add(new BookReply { Title = "Fly me to Paris" });
+            authors.Add(jack);
+        }
+
+        public override Task<AuthorResponse> GetAuthor(AuthorRequest request, ServerCallContext context)
+        {
+            var author = authors.FirstOrDefault(x => x.Name == request.Name);
+            return Task.FromResult(author!);
+        }
+    }
+}
